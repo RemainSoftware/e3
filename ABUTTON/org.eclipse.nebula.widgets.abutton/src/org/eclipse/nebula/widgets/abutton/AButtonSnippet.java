f1ecd356caaa20d4638ee4c717ef5ede9145454f
Wim Jongman <wim.jongman@remainsoftware.com>
2021-10-15 13:49:01 +0200

    AButton - Implementation of a custom SWT widget
    Initial commit

--- ABUTTON/org.eclipse.nebula.widgets.abutton/src/org/eclipse/nebula/widgets/abutton/AButtonSnippet.java
+++ ABUTTON/org.eclipse.nebula.widgets.abutton/src/org/eclipse/nebula/widgets/abutton/AButtonSnippet.java
@@ -17,26 +17,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-public class AButtonSnippet extends Button {
-
-	/**
-	 * Create a automatic button.
-	 * 
-	 * @param parent
-	 * @param style
-	 */
-	public AButtonSnippet(Composite parent, int style) {
-		super(parent, style);
-		addPaintListener(event -> draw(event));
-	}
-
-	private int fAlpha = 100;
-	private int fDelay = 5000;
-	private int fBorderSize = 2;
-	private boolean fIsDrawing;
-	private boolean fIsDamaged;
-	private boolean fCancelled;
-	private boolean fIsDone;
+public class AButtonSnippet {
 
 	public static void main(String[] args) {
 
@@ -58,7 +39,7 @@ public class AButtonSnippet extends Button {
 		Text ln = new Text(shell, SWT.SINGLE | SWT.LEAD | SWT.BORDER);
 		ln.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
-		AButtonSnippet button = new AButtonSnippet(shell, SWT.PUSH);
+		AButton button = new AButton(shell, SWT.PUSH);
 		button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		button.setText("Store in Database");
 		button.addSelectionListener(new SelectionAdapter() {
@@ -91,148 +72,5 @@ public class AButtonSnippet extends Button {
 		}
 		display.dispose();
 
-	}
-
-	protected boolean isCancelled() {
-		return fCancelled;
-	}
-
-	/**
-	 * Sets the transparency of the animation.
-	 * 
-	 * @param alpha the transparency value between 0 and 255l
-	 * @return this
-	 */
-	public AButtonSnippet setAlpha(int alpha) {
-		fAlpha = alpha;
-		return this;
-	}
-
-	/**
-	 * @return the transparency value of the animation overlay.
-	 */
-	public int getAlpha() {
-		return fAlpha;
-	}
-
-	/**
-	 * Cancel the countdown of the widget.
-	 * 
-	 * @return this
-	 */
-	public AButtonSnippet cancel() {
-		fCancelled = true;
-		return this;
-	}
-
-	/**
-	 * Restart the countdown.
-	 * 
-	 * @return this
-	 */
-	public AButtonSnippet reStart() {
-		fCancelled = false;
-		fIsDone = false;
-		redraw();
-		return this;
-	}
-
-	/**
-	 * Sets the delay after which the button will automatically press itself.
-	 * 
-	 * @param delay the delay in milliseconds
-	 * @return this
-	 */
-	public AButtonSnippet setDelay(int delay) {
-		fDelay = delay;
-		return this;
-	}
-
-	/**
-	 * @return the delay to the automatic press in milliseconds.
-	 */
-	public int getDelay() {
-		return fDelay;
-	}
-
-	public AButtonSnippet setBorderSize(int borderSize) {
-		fBorderSize = borderSize;
-		return this;
-	}
-
-	public int getBorderSize() {
-		return fBorderSize;
-	}
-
-	private void draw(PaintEvent event) {
-		GC gc = new GC((AButtonSnippet) event.widget);
-		AButtonSnippet button = (AButtonSnippet) event.widget;
-		Rectangle bounds = button.getBounds();
-		Thread drawThread = new Thread(() -> {
-			doDraw(gc, button, bounds);
-		});
-		drawThread.start();
-	}
-
-	private void doDraw(GC gc, AButtonSnippet button, Rectangle bounds) {
-
-		gc.setAlpha(button.getAlpha());
-		int sleepTime = button.getDelay() / bounds.width;
-		int borderWidth = getBorderSize();
-
-		if (fIsDone) {
-			drawDamagedArea(gc, bounds, borderWidth, bounds.width);
-			return;
-		}
-
-		if (fIsDrawing) {
-			fIsDamaged = true;
-			return;
-		}
-		try {
-			fIsDrawing = true;
-
-			for (int i = borderWidth; i < bounds.width - borderWidth; i++) {
-				if (fCancelled) {
-					break;
-				}
-				if (fIsDamaged && i > borderWidth) {
-					drawDamagedArea(gc, bounds, borderWidth, i);
-					fIsDamaged = false;
-				}
-				gc.drawLine(i, borderWidth, i, bounds.height - borderWidth);
-				sleep(sleepTime);
-			}
-			fire();
-		} finally {
-			fIsDrawing = false;
-			fIsDone = true;
-		}
-	}
-
-	private void drawDamagedArea(GC gc, Rectangle bounds, int borderWidth, int width) {
-		Color background = gc.getBackground();
-		gc.setBackground(gc.getForeground());
-		gc.fillRectangle(borderWidth, borderWidth, width - 1, bounds.height - borderWidth - 1);
-		gc.setBackground(background);
-	}
-
-	private void fire() {
-		getDisplay().asyncExec(() -> {
-			// setSelection(true);
-			notifyListeners(SWT.Selection, new Event());
-		});
-	}
-
-	private static void sleep(int sleepTimeMillis) {
-		try {
-			Thread.sleep(sleepTimeMillis);
-		} catch (InterruptedException e) {
-			// nop
-		}
-	}
-
-	@Override
-	protected void checkSubclass() {
 	}
 }
