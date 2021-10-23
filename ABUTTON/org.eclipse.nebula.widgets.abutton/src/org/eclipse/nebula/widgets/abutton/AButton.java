f557434bec9bce80c5b5101adda60780e81dc79d
Wim Jongman <wim.jongman@remainsoftware.com>
2021-10-23 18:12:40 +0200

    AButton - Implementation of a custom SWT widget
    Minor changes

--- ABUTTON/org.eclipse.nebula.widgets.abutton/src/org/eclipse/nebula/widgets/abutton/AButton.java
+++ ABUTTON/org.eclipse.nebula.widgets.abutton/src/org/eclipse/nebula/widgets/abutton/AButton.java
@@ -1,115 +1,97 @@
-/*******************************************************************************
- * Copyright (c) 2021, 2021 Remain Software and others
- *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     Wim Jongman - initial API and implementation
- *******************************************************************************/
 package org.eclipse.nebula.widgets.abutton;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 
-/**
- * 
- * A timer button inspired by the NetFlix automatic button.
- *
- */
 public class AButton extends Button {
 
-	/**
-	 * Create a automatic button.
-	 * 
-	 * @param parent
-	 * @param style
-	 */
-	public AButton(Composite parent, int style) {
-		super(parent, style);
-		addPaintListener(event -> draw(event));
-	}
-
-	private int fAlpha = 100;
-	private int fDelay = 5000;
 	private int fBorderSize = 2;
+	private int fAnimationTime = 5000;
 	private boolean fIsDrawing;
 	private boolean fIsDamaged;
-	private boolean fCancelled;
-	private boolean fIsDone;
-	private boolean fIsStarted;
+	private boolean fCanRun = false;
 
-	protected boolean isCancelled() {
-		return fCancelled;
+	public AButton(Composite parent, int style) {
+		super(parent, style);
+		addPaintListener(e -> draw(e));
+	}
+
+	private void draw(PaintEvent e) {
+		if (fIsDrawing) {
+			fIsDamaged = true;
+			return;
+		}
+		if (!fCanRun) {
+			return;
+		}
+		Thread thread = new Thread(getAnimationRunner(e));
+		thread.setName("Automatic button animation runner");
+		thread.start();
+	}
+
+	private Runnable getAnimationRunner(PaintEvent e) {
+		int bs = getBorderSize();
+		int width = getSize().x;
+		int height = getSize().y;
+		fCanRun = false;
+		GC gc = new GC(this);
+		gc.setAlpha(100);
+		Color fgColor = getForeground();
+		gc.setBackground(fgColor);
+		return () -> {
+			try {
+				fIsDrawing = true;
+				int lines = width - bs;
+				for (int i = bs; i < lines; i++) {
+					gc.drawLine(i, bs, i, height - bs - 1);
+					sleep(getAnimationTime() / lines);
+					if (isDisposed()) {
+						break;
+					}
+					if (fIsDamaged) {
+						gc.fillRectangle(bs, bs, i - 2, height - (2 * bs));
+						fIsDamaged = false;
+					}
+				}
+				if (!isDisposed()) {
+					getDisplay().asyncExec(() -> {
+						setSelection(true);
+						notifyListeners(SWT.Selection, new Event());
+					});
+				}
+			} finally {
+				fIsDrawing = false;
+			}
+		};
+	}
+
+	private void sleep(int sleepTime) {
+		try {
+			Thread.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			// don't care
+		}
 	}
 
 	/**
-	 * Sets the transparency of the animation.
-	 * 
-	 * @param alpha the transparency value between 0 and 255l
-	 * @return this
+	 * @param animationTime the animation time in milliseconds
 	 */
-	public AButton setAlpha(int alpha) {
-		fAlpha = alpha;
+	public AButton setAnimationTime(int animationTime) {
+		fAnimationTime = animationTime;
 		return this;
 	}
 
-	/**
-	 * @return the transparency value of the animation overlay.
-	 */
-	public int getAlpha() {
-		return fAlpha;
+	public int getAnimationTime() {
+		return fAnimationTime;
 	}
 
-	/**
-	 * Cancel the countdown of the widget.
-	 * 
-	 * @return this
-	 */
-	public AButton cancel() {
-		fCancelled = true;
-		return this;
-	}
-
-	/**
-	 * Restart the countdown.
-	 * 
-	 * @return this
-	 */
-	public AButton start() {
-		fCancelled = false;
-		fIsDone = false;
-		fIsStarted = true;
-		redraw();
-		return this;
-	}
-
-	/**
-	 * Sets the delay after which the button will automatically press itself.
-	 * 
-	 * @param delay the delay in milliseconds
-	 * @return this
-	 */
-	public AButton setDelay(int delay) {
-		fDelay = delay;
-		return this;
-	}
-
-	/**
-	 * @return the delay to the automatic press in milliseconds.
-	 */
-	public int getDelay() {
-		return fDelay;
+	public int getBorderSize() {
+		return fBorderSize;
 	}
 
 	public AButton setBorderSize(int borderSize) {
@@ -117,78 +99,12 @@ public class AButton extends Button {
 		return this;
 	}
 
-	public int getBorderSize() {
-		return fBorderSize;
-	}
-
-	private void draw(PaintEvent event) {
-		GC gc = new GC((AButton) event.widget);
-		AButton button = (AButton) event.widget;
-		Rectangle bounds = button.getBounds();
-		Thread drawThread = new Thread(() -> {
-			doDraw(gc, button, bounds);
-		});
-		drawThread.start();
-	}
-
-	private void doDraw(GC gc, AButton button, Rectangle bounds) {
-
-		gc.setAlpha(button.getAlpha());
-		int sleepTime = button.getDelay() / bounds.width;
-		int borderWidth = getBorderSize();
-
-		if (fIsDone || !fIsStarted) {
-			if (fIsStarted) {
-				drawDamagedArea(gc, bounds, borderWidth, bounds.width);
-			}
-			return;
-		}
-
-		if (fIsDrawing) {
-			fIsDamaged = true;
-			return;
-		}
-		try {
-			fIsDrawing = true;
-
-			for (int i = borderWidth; i < bounds.width - borderWidth; i++) {
-				if (fCancelled) {
-					break;
-				}
-				if (fIsDamaged && i > borderWidth) {
-					drawDamagedArea(gc, bounds, borderWidth, i);
-					fIsDamaged = false;
-				}
-				gc.drawLine(i, borderWidth, i, bounds.height - borderWidth);
-				sleep(sleepTime);
-			}
-			fire();
-		} finally {
-			fIsDrawing = false;
-			fIsDone = true;
-		}
-	}
-
-	private void drawDamagedArea(GC gc, Rectangle bounds, int borderWidth, int width) {
-		Color background = gc.getBackground();
-		gc.setBackground(gc.getForeground());
-		gc.fillRectangle(borderWidth, borderWidth, width - 2, bounds.height - borderWidth);
-		gc.setBackground(background);
-	}
-
-	private void fire() {
-		getDisplay().asyncExec(() -> {
-			// setSelection(true);
-			notifyListeners(SWT.Selection, new Event());
-		});
-	}
-
-	private static void sleep(int sleepTimeMillis) {
-		try {
-			Thread.sleep(sleepTimeMillis);
-		} catch (InterruptedException e) {
-			// nop
-		}
+	/**
+	 * Starts the animation.
+	 */
+	public void start() {
+		fCanRun = true;
+		getDisplay().asyncExec(() -> redraw());
 	}
 
 	@Override
--- ABUTTON/org.eclipse.nebula.widgets.abutton/src/org/eclipse/nebula/widgets/abutton/AButtonSnippet.java
+++ ABUTTON/org.eclipse.nebula.widgets.abutton/src/org/eclipse/nebula/widgets/abutton/AButtonSnippet.java
@@ -1,51 +1,34 @@
 package org.eclipse.nebula.widgets.abutton;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 public class AButtonSnippet {
-
-	private static Text text;
 
 	public static void main(String[] args) {
 
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setLayout(new GridLayout(1, true));
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		shell.setSize(600, 100);
 
-		shell.setSize(600, 600);
+		int width = display.getBounds().width;
+		int height = display.getBounds().height;
 
-		AButton button = new AButton(shell, SWT.PUSH);
-		button.cancel();
-		button.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		button.setText("Automatic Button");
-		button.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				text.setText(text.getText() + " Button fired");
-			}
-		});
+		shell.setLocation(width / 2 - (600 / 2), height / 2 - (300 / 2));
 
-		Button button2 = new Button(shell, SWT.PUSH);
-		button2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		button2.setText("Start");
-		button2.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				text.setText("");
-				button.start();
-			}
-		});
+		AButton aButton = new AButton(shell, SWT.PUSH);
+		aButton.setText("> Next Episode");
+		aButton.addListener(SWT.Selection, e -> System.out.println("Triggered..."));
+		aButton.setAnimationTime(10000) //
+				.setBorderSize(3);
 
-		text = new Text(shell, SWT.MULTI | SWT.WRAP | SWT.LEAD | SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		Button button = new Button(shell, SWT.PUSH);
+		button.setText("Activate");
+		button.addListener(SWT.Selection, e -> aButton.start());
 
 		shell.open();
 		while (!shell.isDisposed()) {
